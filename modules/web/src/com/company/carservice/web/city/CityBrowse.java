7be6a699fb26e88bd66eb895ad90a95942c4e9bf
magomedov <alx.mag@ya.ru>
2018-12-17 13:17:08 +0400

    Legacy screen for Cities

--- modules/web/src/com/company/carservice/web/city/CityBrowse.java
+++ modules/web/src/com/company/carservice/web/city/CityBrowse.java
@@ -1,15 +1,6 @@
 package com.company.carservice.web.city;
 
-import com.company.carservice.entity.City;
-import com.haulmont.cuba.gui.screen.*;
+import com.haulmont.cuba.gui.components.AbstractLookup;
 
-
-@UiController("carservice_City.browse")
-@UiDescriptor("city-browse.xml")
-@LookupComponent("citiesTable")
-@LoadDataBeforeShow
-public class CityBrowse extends StandardLookup<City> {
-
-    
-
+public class CityBrowse extends AbstractLookup {
 }
--- modules/web/src/com/company/carservice/web/city/CityEdit.java
+++ modules/web/src/com/company/carservice/web/city/CityEdit.java
@@ -1,12 +1,7 @@
 package com.company.carservice.web.city;
 
-import com.haulmont.cuba.gui.screen.*;
+import com.haulmont.cuba.gui.components.AbstractEditor;
 import com.company.carservice.entity.City;
 
-
-@UiController("carservice_City.edit")
-@UiDescriptor("city-edit.xml")
-@EditedEntityContainer("cityDc")
-@LoadDataBeforeShow
-public class CityEdit extends StandardEditor<City> {
+public class CityEdit extends AbstractEditor<City> {
 }
