5901b7a6bcc7039c40a2c1b7e7358e705294a28a
alx-mag <45179557+alx-mag@users.noreply.github.com>
2018-12-19 12:43:42 +0400

    Localisation

--- modules/web/src/com/company/carservice/web/carservicecenter/CarServiceCenterEdit.java
+++ modules/web/src/com/company/carservice/web/carservicecenter/CarServiceCenterEdit.java
@@ -1,14 +1,11 @@
 package com.company.carservice.web.carservicecenter;
 
-import com.company.carservice.entity.City;
-import com.company.carservice.entity.Customer;
-import com.company.carservice.entity.Employee;
+import com.company.carservice.entity.*;
 import com.company.carservice.service.CityService;
-import com.haulmont.cuba.gui.components.AbstractEditor;
-import com.company.carservice.entity.CarServiceCenter;
-import com.haulmont.cuba.gui.components.Component;
-import com.haulmont.cuba.gui.components.TabSheet;
-import com.haulmont.cuba.gui.components.Table;
+import com.haulmont.cuba.core.app.LocalizedMessageService;
+import com.haulmont.cuba.core.global.MessageTools;
+import com.haulmont.cuba.gui.UiComponents;
+import com.haulmont.cuba.gui.components.*;
 import com.haulmont.cuba.gui.data.CollectionDatasource;
 
 import javax.inject.Inject;
@@ -20,6 +17,12 @@ public class CarServiceCenterEdit extends AbstractEditor<CarServiceCenter>
 {
     @Inject
     private TabSheet tabSheet;
+
+    @Inject
+    private Table<Customer> customerTable;
+
+    @Inject
+    private MessageTools messageTools;
 
     @Inject
     private CollectionDatasource<Customer, UUID> customerDs;
@@ -36,6 +39,24 @@ public class CarServiceCenterEdit extends AbstractEditor<CarServiceCenter>
     @Override
     public void init(Map<String, Object> params) {
         customerDs.addCollectionChangeListener(event -> initCustomersTabCaption());
+
+        customerTable.addGeneratedColumn("clientType", this::createClientTypeLabel);
+        String clientTypeCaption = messageTools.loadString("msg://com.company.carservice.entity/Customer.clientType");
+        customerTable.getColumn("clientType").setCaption(clientTypeCaption);
+    }
+
+    private Component createClientTypeLabel(Customer customer) {
+        return new Table.PlainTextCell(getCustomerLabel(customer));
+    }
+
+    private String getCustomerLabel(Customer customer) {
+        if (customer instanceof Individual) {
+            return messageTools.loadString("msg://com.company.carservice.entity/Individual");
+        } else if (customer instanceof Company) {
+            return messageTools.loadString("msg://com.company.carservice.entity/Company");
+        } else {
+            throw new IllegalArgumentException("Unknown customer type");
+        }
     }
 
     @Override
